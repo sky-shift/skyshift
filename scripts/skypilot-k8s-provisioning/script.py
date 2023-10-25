8842ac4b45db5f7518cf29284fa56e399def288a
Justin Chang <changguangyu@yahoo.com>
2023-10-24 18:36:06 -0700

    individual launches

--- scripts/skypilot-k8s-provisioning/script.py
+++ scripts/skypilot-k8s-provisioning/script.py
@@ -1,32 +1,77 @@
 import os
+import threading
 
 import sky
 import yaml
 
 
 def provision_resources(num_nodes, cluster_name, run_command, resources):
-    task = sky.Task(num_nodes=num_nodes,
-                    setup='echo "setup"',
-                    run=run_command)
-    ports = ["22", "6443", "2376", "2379", "2380", "8472", "9099",
-             "10250", "443", "379", "6443", "8472", "80", "472", "10254", "3389", "30000-32767"]
-    if "ports" not in resources:
-        resources["ports"] = ports
-    else:
-        resources["ports"] = list(set(resources["ports"] + ports))
-    task.set_resources(sky.Resources(**resources))
-    sky.launch(task, cluster_name,)
+    """
+    Provision resources for the cluster using SkyPilot Python API.
+    """
+    def create_resource(num):
+        task = sky.Task(setup='echo "setup"',
+                        run=run_command)
+        ports = ["22", "6443", "2376", "2379", "2380", "8472", "9099",
+                 "10250", "443", "379", "6443", "8472", "80", "472", "10254", "3389", "30000-32767"]
+        if "ports" not in resources:
+            resources["ports"] = ports
+        else:
+            resources["ports"] = list(set(resources["ports"] + ports))
+        task.set_resources(sky.Resources(**resources))
+        sky.launch(task, f"skym-{cluster_name}-{num}",)
+
+    threads = []
+    for i in range(num_nodes):
+        thread = threading.Thread(target=create_resource, args=(i,))
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
 
 
-def construct_cluster_yaml(ips):
+def parse_ssh_config():
+    """
+    Parse the SSH config file and return a dictionary mapping hosts to their users.
+    """
+    with open(os.path.expanduser("~/.ssh/config"), 'r') as f:
+        lines = f.readlines()
+
+    current_host = None
+    host_details = {}
+    prev_line = None
+
+    for line in lines:
+        line = line.strip()
+
+        if prev_line and prev_line.startswith("# Added by sky") and line.startswith('Host '):
+            current_host = line.split()[1]
+            host_details[current_host] = {}
+        elif current_host:
+            if line.startswith('HostName '):
+                host_details[current_host]['hostname'] = line.split()[1]
+            elif line.startswith('User '):
+                host_details[current_host]['user'] = line.split()[1]
+
+        prev_line = line
+
+    return host_details
+
+
+def construct_cluster_yaml(cluster_name, num_nodes):
+    """
+    Construct the cluster.yml file for RKE.
+    """
     data = {"nodes": []}
-    for i in range(len(ips)):
-        ip = ips[i]
-
+    host_details = parse_ssh_config()
+    print(host_details)
+    for i in range(num_nodes):
+        details = host_details[f"skym-{cluster_name}-{i}"]
         node = {
-            "address": ip,
-            "user": "gcpuser",  # TODO: auto detect
-            "ssh_key_path": "~/.ssh/sky-key",
+            "address": details["hostname"],
+            "user": details["user"],
+            "ssh_key_path": os.path.expanduser("~/.ssh/sky-key"),
             "ssh_port": 22,
             "role": ["worker"]
         }
@@ -39,13 +84,11 @@ def construct_cluster_yaml(ips):
     with open('cluster.yml', 'w') as file:
         yaml.dump(data, file)
 
-    os.system("rke up --config cluster.yml")
-
 
 def script(cluster_name, num_nodes, run, resources={}):
     provision_resources(num_nodes, cluster_name, run, resources)
-    ips = sky.status(cluster_name, True)[0]['handle'].external_ips()
-    construct_cluster_yaml(ips)
+    construct_cluster_yaml(cluster_name, num_nodes)
+    os.system("rke up --config cluster.yml")
 
 
 script("test", 2, 'echo "Stopping Ray"')
