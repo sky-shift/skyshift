0c03e5eb4a34fad36bbc120acfdeaa1d99e9dbbc
Michael Luo <michael.luo@berkeley.edu>
2023-12-12 02:11:15 +0000

    Automatically install etcd upon installation

--- setup.py
+++ setup.py
@@ -1,9 +1,41 @@
-from setuptools import setup
+import requests
+from setuptools import setup, Command
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+import subprocess
+
+
+def check_and_install_etcd():
+    try:
+        subprocess.run(["etcd", "--version"], check=True)
+        print("[Preinstaller] ETCD is running.")
+    except requests.ConnectionError:
+        print("[Preinstaller] ETCD is not running, automatically installing ETCD.")
+        # Install ETCD (platform-specific code goes here)
+        # Start ETCD in background (platform-specific code goes here)
+        subprocess.call(['./etcd_installation.sh'])
+
+
+class CustomDevelopCommand(develop):
+    def run(self):
+        check_and_install_etcd()
+        develop.run(self)
+
+
+class CustomInstallCommand(install):
+    def run(self):
+        check_and_install_etcd()
+        install.run(self)
+
 
 setup(name='sky_manager',
       version='0.0',
       description='Sky Manager',
       packages=['sky_manager'],
+      cmdclass={
+        'install': CustomInstallCommand,
+        'develop': CustomDevelopCommand,
+      },
       install_requires=[
           'Click',
       ],
