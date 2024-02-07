d2202e221357ed38ce67d2228c8629764a292cf9
Alexmontoril23 <alex.cl.2000@gmail.com>
2024-02-07 18:54:48 +0100

    Implemented tests for API client (pytest based). Found bugs related to unexpected responses and adjusted existing logic accordingly.

--- skyflow/api_client/object_api.py
+++ skyflow/api_client/object_api.py
@@ -34,11 +34,6 @@ def verify_response(input_data):
     return load_object(body)
 
 
-def process_name(name: str):
-    """Processes the name of an object for http request."""
-    return quote(name, safe='')
-
-
 # @TODO(mluo): Introduce different types of API exceptions.
 class APIException(Exception):
     """Generic API Exception."""
@@ -86,6 +81,9 @@ class NamespaceObjectAPI(ObjectAPI):
         if namespace:
             self.url = f"http://{self.host}:{self.port}/{self.namespace}/{self.object_type}"
         else:
+        if namespace:
+            self.url = f"http://{self.host}:{self.port}/{self.namespace}/{self.object_type}"
+        else:
             self.url = f"http://{self.host}:{self.port}/{self.object_type}"
 
     def create(self, config: dict):
@@ -108,14 +106,12 @@ class NamespaceObjectAPI(ObjectAPI):
 
     def get(self, name: str):
         assert self.namespace, "Method `get` requires a namespace."
-        processed_name = process_name(name)
-        response = requests.get(f"{self.url}/{processed_name}")
+        response = requests.get(f"{self.url}/{name}")
         return verify_response(response)
 
     def delete(self, name: str):
         assert self.namespace, "Method `delete` requires a namespace."
-        processed_name = process_name(name)
-        response = requests.delete(f"{self.url}/{processed_name}")
+        response = requests.delete(f"{self.url}/{name}")
         return verify_response(response)
 
     def watch(self):
@@ -153,13 +149,11 @@ class NoNamespaceObjectAPI(ObjectAPI):
         return verify_response(response)
 
     def get(self, name: str):
-        processed_name = process_name(name)
-        response = requests.get(f"{self.url}/{processed_name}")
+        response = requests.get(f"{self.url}/{name}")
         return verify_response(response)
 
     def delete(self, name: str):
-        processed_name = process_name(name)
-        response = requests.delete(f"{self.url}/{processed_name}")
+        response = requests.delete(f"{self.url}/{name}")
         return verify_response(response)
 
     def watch(self):
