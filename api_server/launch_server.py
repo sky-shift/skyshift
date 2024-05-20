sky-shift/skyshift

commit 9f2a90d91a53ed7bb654ce1ef709b5dec664bab6

Add option to make SkyFlow publically accessible from any machine (#205)
* init push

* fix imports

* format.sh

api_server/launch_server.py
@@ -7,6 +7,7 @@ import os
 import subprocess
 from typing import Optional
 
+import requests
 import uvicorn
 from etcd3.exceptions import ConnectionFailedError
 
@@ -15,7 +16,7 @@ from skyflow.etcd_client.etcd_client import ETCDClient
 from skyflow.utils.utils import (generate_manager_config,
                                  generate_temp_directory)
 
-API_SERVER_HOST = "127.0.0.1"
+API_SERVER_HOST = "0.0.0.0"
 API_SERVER_PORT = 50051
 
 
@@ -61,6 +62,7 @@ def main(
     # Check if etcd is installed and running - elsewise, install and launch etcd.
     if not check_and_install_etcd(data_directory):
         return
+
     generate_manager_config(host, port)
     #Create temperorary directory used for worker sync
     generate_temp_directory(CONF_FLAG_DIR)

setup.py
@@ -6,32 +6,37 @@ setup(
     description="Sky Manager",
     packages=["skyflow"],
     install_requires=[
+        "bcrypt==4.0.1",
         "Click",
         "click_aliases",
         "etcd3",
+        "fastapi",
         "kubernetes",
         "passlib",
         "protobuf==3.20.0",
         "python-jose",
         "pydantic>=2.5.0",
+        "pydantic[email]>=2.5.0",
         'python-multipart',
         "pytest",
         "pyyaml",
         "requests>=2.31.0",
         "rich",
+        "skypilot==0.5.0",
         "tabulate",
         "types-passlib",
         "types-psutil",
         "types-tabulate",
         "types-PyYAML",
         "types-requests",
-        "skypilot==0.5.0",
-        "bcrypt==4.0.1",
+        "websockets",
     ],
     extras_require={
         "server": [
-            "etcd3", "fastapi", "uvicorn[standard]", "pydantic[email]>=2.5.0",
-            'jsonpatch', "pyjwt",
+            "etcd3",
+            "uvicorn[standard]",
+            'jsonpatch',
+            "pyjwt",
         ],
         "dev": [
             "yapf==0.32.0", "pylint==2.8.2", "pylint-quotes==0.2.3",

skyflow/utils/utils.py
@@ -42,6 +42,11 @@ def generate_manager_config(host: str, port: int):
     """Generates the API server config file."""
     absolute_path = os.path.expanduser(API_SERVER_CONFIG_PATH)
 
+    if host == '0.0.0.0':
+        # Fetch public IP address of the machine.
+        response = requests.get('https://api.ipify.org?format=json')
+        host = response.json()['ip']
+
     # If path exists, check if host and port are identical
     if os.path.exists(absolute_path):
         with open(absolute_path, "r") as config_file:
@@ -64,6 +69,7 @@ def generate_manager_config(host: str, port: int):
     os.makedirs(os.path.dirname(absolute_path), exist_ok=True)
     with open(absolute_path, "w") as config_file:
         yaml.dump(config_dict, config_file)
+
 
 def generate_temp_directory(directory_path):
     """

commit 7c183a7d340cb2137e791f0114f34453e2024cf6

fixed default host issue caused by PR #205

api_server/launch_server.py
@@ -16,7 +16,7 @@ from skyflow.etcd_client.etcd_client import ETCDClient
 from skyflow.utils.utils import (generate_manager_config,
                                  generate_temp_directory)
 
-API_SERVER_HOST = "0.0.0.0"
+API_SERVER_HOST = "127.0.0.1"
 API_SERVER_PORT = 50051
 
 