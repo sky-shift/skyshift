3d309d08da0cbf32089513b1a6913721ecb2b9a1
Alexmontoril23 <alex.cl.2000@gmail.com>
2024-05-25 11:41:20 +0200

    Improved code for fuzzy_map_gpu

--- skyflow/utils/utils.py
+++ skyflow/utils/utils.py
@@ -4,6 +4,7 @@ Utility functions for Skyflow.
 import importlib
 import json
 import os
+import re
 import shutil
 from datetime import datetime, timezone
 from typing import Dict, List, Optional, Union
@@ -13,9 +14,25 @@ import yaml
 from rapidfuzz import process
 
 from skyflow.globals import API_SERVER_CONFIG_PATH, SKYCONF_DIR
-from skyflow.templates.resource_template import AcceleratorEnum
+from skyflow.templates.resource_template import AcceleratorEnum, ResourceEnum
 
 OBJECT_TEMPLATES = importlib.import_module("skyflow.templates")
+
+
+def parse_resource_cpu(resource_str):
+    """Parse CPU string to cpu count."""
+    unit_map = {"m": 1e-3, "K": 1e3}
+    value = re.search(r"\d+", resource_str).group()
+    unit = resource_str[len(value):]
+    return float(value) * unit_map.get(unit, 1)
+
+
+def parse_resource_memory(resource_str):
+    """Parse resource string to megabytes."""
+    unit_map = {"Ki": 2**10, "Mi": 2**20, "Gi": 2**30, "Ti": 2**40}
+    value = re.search(r"\d+", resource_str).group()
+    unit = resource_str[len(value):]
+    return float(value) * unit_map.get(unit, 1) / (2**20)
 
 
 def sanitize_cluster_name(value: str) -> str:
@@ -197,10 +214,11 @@ def fuzzy_map_gpu(
     Maps GPUs to the closest match in the enum class using rapidfuzz.
     """
     enum_values = [e.value for e in AcceleratorEnum]
+    non_gpu_keys = {e.value for e in ResourceEnum}
+
     for _, resources in resources_dict.items():
         for key in list(resources.keys()):
-            if key not in AcceleratorEnum.__members__:
-                # Find the closest match
+            if key not in non_gpu_keys:
                 closest_match = process.extractOne(key, enum_values)
                 if closest_match:
                     resources[closest_match[0]] = resources.pop(key)
