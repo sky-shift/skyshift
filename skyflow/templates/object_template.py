ac75bef83632ca7402c1a65b313ee38465fcd21d
Alejandro Cuadron Lafuente <alex.cl.2000@gmail.com>
2024-06-04 02:29:01 +0200

    Fixes skyctl status discrepancy (#241) (#244)
    * fixes issue 241
    
    * fix comments

--- skyflow/templates/object_template.py
+++ skyflow/templates/object_template.py
@@ -3,6 +3,7 @@ Object template.
 """
 import re
 import uuid
+from datetime import datetime
 from typing import Dict, Generic, List, TypeVar
 
 from pydantic import BaseModel, Field, field_validator, model_validator
@@ -93,11 +94,15 @@ class ObjectMeta(BaseModel, validate_assignment=True):
         """
         Validates the creation timestamp.
 
-        If it does not exist, set it to the current time.
+        If it does not exist or is not valid, set it to the current time.
         """
-        if timestamp:
-            return timestamp
-        return fetch_datetime()
+        try:
+            # Try to parse the timestamp to ensure it's valid
+            datetime.fromisoformat(timestamp)
+        except ValueError:
+            # If parsing fails, set to current timestamp
+            timestamp = fetch_datetime()
+        return timestamp
 
 
 class NamespacedObjectMeta(ObjectMeta):
