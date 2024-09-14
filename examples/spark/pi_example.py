9fbd0ba27cdc1dd3f52afe88bddb8db9eb232b03
Arnav Balyan <60175178+ArnavBalyan@users.noreply.github.com>
2024-09-14 15:09:12 +0530

    Add Documentation and Examples for RBAC (#305)
    * Add RBAC doc

--- examples/spark/pi_example.py
+++ examples/spark/pi_example.py
@@ -1,9 +1,10 @@
+# pylint: skip-file
+# Disabling lint since the dependency comes from the docker image provided alongside at runtime
 import sys
-from random import random
 from operator import add
+from random import random
 
 from pyspark.sql import SparkSession
-
 
 if __name__ == "__main__":
     """
@@ -20,9 +21,10 @@ if __name__ == "__main__":
     def f(_: int) -> float:
         x = random() * 2 - 1
         y = random() * 2 - 1
-        return 1 if x ** 2 + y ** 2 <= 1 else 0
+        return 1 if x**2 + y**2 <= 1 else 0
 
-    count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
+    count = spark.sparkContext.parallelize(range(1, n + 1),
+                                           partitions).map(f).reduce(add)
     print("Pi is roughly %f" % (4.0 * count / n))
 
     spark.stop()
