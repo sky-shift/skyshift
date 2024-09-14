d2225f7f14a5517b27eb0ea1788ea999360c0626
Arnav Balyan <arnavbalyan1@gmail.com>
2024-09-14 14:46:19 +0530

    lint

--- examples/spark/pi_example.py
+++ examples/spark/pi_example.py
@@ -4,7 +4,6 @@ from operator import add
 
 from pyspark.sql import SparkSession
 
-
 if __name__ == "__main__":
     """
         Usage: pi [partitions]
@@ -21,7 +20,6 @@ if __name__ == "__main__":
         x = random() * 2 - 1
         y = random() * 2 - 1
         return 1 if x**2 + y**2 <= 1 else 0
-
 
     count = spark.sparkContext.parallelize(range(1, n + 1),
                                            partitions).map(f).reduce(add)
