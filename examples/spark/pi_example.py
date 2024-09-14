ae879fb6c794c4fde75b773eb1fea43fa9bf171a
Arnav Balyan <arnavbalyan1@gmail.com>
2024-09-14 14:42:20 +0530

    fix ci/cd

--- examples/spark/pi_example.py
+++ examples/spark/pi_example.py
@@ -20,9 +20,11 @@ if __name__ == "__main__":
     def f(_: int) -> float:
         x = random() * 2 - 1
         y = random() * 2 - 1
-        return 1 if x ** 2 + y ** 2 <= 1 else 0
+        return 1 if x**2 + y**2 <= 1 else 0
 
-    count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
+
+    count = spark.sparkContext.parallelize(range(1, n + 1),
+                                           partitions).map(f).reduce(add)
     print("Pi is roughly %f" % (4.0 * count / n))
 
     spark.stop()
