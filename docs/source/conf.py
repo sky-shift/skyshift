e4e0a7ce350aa8a0947052cd987d23ab0e08bd09
Arnav Balyan <arnavbalyan1@gmail.com>
2024-09-21 23:24:57 +0530

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,12 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('..'))  # Adjust this path as needed
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
