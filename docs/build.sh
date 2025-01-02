0b19ddd452f73e48e48b23cd4b5fbfaf93733bee
Michael Luo <luomichael@google.com>
2025-01-01 20:07:56 -0800

    Add github docs workflow

--- docs/build.sh
+++ docs/build.sh
@@ -12,6 +12,11 @@ rm -rf build docs
 # Execute make html command
 make html
 
-# Run the server for docs
-cd build/html
-python3 -m http.server 8000
+# Check if the --serve flag is passed
+if [[ "$1" == "--serve" ]]; then
+  # Run the server for docs
+  cd build/html
+  python3 -m http.server 8000
+else
+  echo "Build complete. Use --serve to start a local server."
+fi
